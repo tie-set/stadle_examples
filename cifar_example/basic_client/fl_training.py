d7123c042714f45740f4036792459b1254822827
georgejeno8 <georgejeno8@gmail.com>
2023-02-14 18:13:07 +0900

    Update stadle_examples to work with v0.6.0 client

--- cifar_example/basic_client/fl_training.py
+++ cifar_example/basic_client/fl_training.py
@@ -1,5 +1,7 @@
 import sys
+import argparse
 
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -10,7 +12,10 @@ from vgg import VGG
 
 from stadle import BasicClient
 
-def main():
+
+def data_processing(data_save_path='./cifar_data', batch_size=64, cl_args=None):
+    classes = ('airplane', 'automobile', 'bird', 'cat', 'deer',
+        'dog', 'frog', 'horse', 'ship', 'truck')
 
     transform_train = transforms.Compose([
         transforms.RandomCrop(32, padding=4),
@@ -25,60 +30,59 @@ def main():
     ])
 
     trainset = torchvision.datasets.CIFAR10(
-        root='data', train=True, download=True, transform=transform_train)
+        root=data_save_path, train=True, download=True, transform=transform_train)
+
+    ###
+    # Modification for imbalanced train datasets
+
+    class_counts = int(args.def_count * 5000) * np.ones(len(classes))
+
+    for c in classes:
+        if c in args.classes:
+            class_counts[trainset.class_to_idx[c]] = int(args.sel_count * 5000)
+
+    imbalanced_idx = []
+
+    for i,img in enumerate(trainset):
+        c = img[1]
+        if (class_counts[c] > 0):
+            imbalanced_idx.append(i)
+            class_counts[c] -= 1
+
+    trainset = torch.utils.data.Subset(trainset, imbalanced_idx)
+
+    ###
+
     trainloader = torch.utils.data.DataLoader(
-        trainset, batch_size=64, shuffle=True, num_workers=2)
+        trainset, batch_size=batch_size, shuffle=True, num_workers=2)
 
     testset = torchvision.datasets.CIFAR10(
-        root='data', train=False, download=True, transform=transform_test)
+        root=data_save_path, train=False, download=True, transform=transform_test)
     testloader = torch.utils.data.DataLoader(
-        testset, batch_size=64, shuffle=False, num_workers=2)
+        testset, batch_size=batch_size, shuffle=False, num_workers=2)
 
-    # Choose a device based on your machine
-    device = 'cuda'
-    # device = 'cpu'
+    return trainloader, testloader
 
-    num_epochs = 200
-    lr = 0.001
-    momentum = 0.9
-
-    model = VGG('VGG16').to(device)
-
+def local_training(model, optimizer, device, num_epochs=1):
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(model.parameters(), lr=lr,
-                        momentum=momentum, weight_decay=5e-4)
-    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200)
 
+    model = model.to(device)
 
-    client_config_path = r'config/config_agent.json'
-    stadle_client = BasicClient(config_file=client_config_path, use_cl_args=True)
+    train_loss = 0
+    correct = 0
+    total = 0
 
-    stadle_client.set_bm_obj(model)
+    model.train()
 
     for epoch in range(num_epochs):
-        if (epoch % 2 == 0):
-            # Don't send model at beginning of training
-            if (epoch != 0):
-                stadle_client.send_trained_model(model)
-
-            state_dict = stadle_client.wait_for_sg_model().state_dict()
-            model.load_state_dict(state_dict)
-
-        print('\nEpoch: %d' % (epoch + 1))
-
-        model = model.to(device)
-
-        model.train()
-        train_loss = 0
-        correct = 0
-        total = 0
-
+        print(f'Epoch {epoch+1}/{num_epochs}')
         for batch_idx, (inputs, targets) in enumerate(trainloader):
             inputs, targets = inputs.to(device), targets.to(device)
 
             optimizer.zero_grad()
             outputs = model(inputs)
             loss = criterion(outputs, targets)
+            train_loss += loss.item()
 
             loss.backward()
             optimizer.step()
@@ -90,27 +94,68 @@ def main():
             sys.stdout.write('\r'+f"\rEpoch Accuracy: {(100*correct/total):.2f}%")
         print('\n')
 
-        if ((epoch + 0) % 5 == 0):
-            model.eval()
-            test_loss = 0
-            correct = 0
-            total = 0
+    train_acc = correct/total
 
-            with torch.no_grad():
-                for batch_idx, (inputs, targets) in enumerate(testloader):
-                    inputs, targets = inputs.to(device), targets.to(device)
-                    outputs = model(inputs)
-                    loss = criterion(outputs, targets)
+    model.eval()
+    test_loss = 0
+    correct = 0
+    total = 0
 
-                    test_loss += loss.item()
-                    _, predicted = outputs.max(1)
-                    total += targets.size(0)
-                    correct += predicted.eq(targets).sum().item()
+    with torch.no_grad():
+        for batch_idx, (inputs, targets) in enumerate(testloader):
+            inputs, targets = inputs.to(device), targets.to(device)
+            outputs = model(inputs)
+            loss = criterion(outputs, targets)
 
-            acc = 100.*correct/total
-            print(f"Accuracy on val set: {acc}%")
+            test_loss += loss.item()
+            _, predicted = outputs.max(1)
+            total += targets.size(0)
+            correct += predicted.eq(targets).sum().item()
+
+    test_acc = correct/total
+    print(f"Accuracy on validation set: {(100*test_acc):.2f}%")
+
+    return train_loss / (len(trainloader) * num_epochs), train_acc, test_loss / len(testloader), test_acc
 
 
-if __name__ == '__main__':
-    main()
+if (__name__ == '__main__'):
+    parser = argparse.ArgumentParser(description='STADLE CIFAR-10 Training')
+    parser.add_argument('--agent_name', default='pytorch_agent')
+    parser.add_argument('--num_rounds', type=int, default=20)
+    parser.add_argument('--def_count', default=0.1, type=float)
+    parser.add_argument('--sel_count', default=1.0, type=float)
+    parser.add_argument('--classes', default=[], nargs='*')
+
+    parser.add_argument('--device', default='cpu')
+
+    args = parser.parse_args()
+
+    client_config_file = 'client_config.json'
+    stadle_client = BasicClient(config_file=client_config_file, agent_name=args.agent_name)
+
+    trainloader, testloader = data_processing(cl_args=args)
+
+    model = VGG('VGG16')
+
+    optimizer = optim.Adam(model.parameters(), lr=1e-3)
+
+    for rnd in range(args.num_rounds):
+        # Update the local model with the aggregate model weights
+        state_dict = stadle_client.wait_for_sg_model().state_dict()
+        model.load_state_dict(state_dict)
+
+        train_loss, train_acc, valid_loss, valid_acc = local_training(model, optimizer, args.device, num_epochs=2)
+        
+        perf_metrics = {
+            'performance': train_acc,
+            'accuracy': valid_acc,
+            'loss_training': train_loss,
+            'loss_test': valid_loss,
+        }
+
+        # Send the locally trained model with the associated performance metric values
+        stadle_client.send_trained_model(model, perf_values=perf_metrics)
+        
+        print(f'Sending model for round {rnd+1} to aggregator')
+
     stadle_client.disconnect()
--- minimal_examples/keras/minimal_fl_agent.py
+++ minimal_examples/keras/minimal_fl_agent.py
@@ -1,18 +1,12 @@
-import os
-import sys
 import argparse
-import time
 import random
-import numpy as np
-import tensorflow as tf
-from typing import List
-from tensorflow import keras
-from stadle import IntegratedClient
+from tf import keras
+from stadle import BasicClient
 
 # from minimal_model import MinimalModel
 
 def get_minimal_model():
-    model = tf.keras.models.Sequential([
+    model = keras.models.Sequential([
         keras.layers.Dense(5, activation='relu', input_shape=(3,)),
         keras.layers.Dense(4)
     ])
@@ -25,84 +19,35 @@ def get_minimal_model():
 
     return model
 
-def train(model, data, **kwargs):
-    # Sleep for 15 seconds to mimic local training
-    # This would be replaced by model training code
-    time.sleep(15)
-    print("Completed training")
+parser = argparse.ArgumentParser(description='STADLE Minimal Training')
+parser.add_argument('--agent_name', default='keras_agent')
+parser.add_argument('--num_rounds', type=int, default=20)
+args = parser.parse_args()
 
-    # Set trained model to original model for minimal example
-    trained_model = model
-    # Example metric (average loss) that the training function can return
-    ave_loss = 0
-
-    return trained_model, ave_loss
+client_config_file = 'client_config.json'
+stadle_client = BasicClient(config_file=client_config_file, agent_name=args.agent_name)
 
 
-def test(test_model, data, **kwargs):
-    # Performance computation is not included in minimal example - the
-    # implementation of this method is dependent on the data and model
-    # that is being trained.
+model = get_minimal_model()
 
-    # Example performance metrics (accuracy and average loss) that can
-    # be computed
-    acc = random.random()
-    ave_loss = 10 * random.random()
+for rnd in range(args.num_rounds):
+    # Update the local model with the aggregate model weights
+    state_dict = stadle_client.wait_for_sg_model().state_dict()
+    model.load_state_dict(state_dict)
 
-    return acc, ave_loss
+    # This is where the local training would occur
+    # Performance metrics would be measured and passed back with the model to the aggregator
+    perf_metrics = {
+        'performance': random.random(),
+        'accuracy': random.random(),
+        'loss_training': random.random(),
+        'loss_valid': random.random(),
+        'loss_test': random.random()
+    }
 
+    # Send the locally trained model with the associated performance metric values
+    stadle_client.send_trained_model(model, perf_values=perf_metrics)
 
-def validate(model, data, **kwargs):
-    print("Validate Model")
-    acc, ave_loss = test(test_model=model, data=data, **kwargs)
-    print(f'Validation Accuracy of the model: {acc} %')
-    return acc, ave_loss
+    print(f'Sending model for round {rnd+1} to aggregator')
 
-
-def judge_termination(**kwargs) -> bool:
-    """
-    Decide if it finishes training process and exits from FL platform
-    :param training_count: int - the number of training done
-    :param sg_arrival_count: int - the number of times it received SG models
-    :return: bool - True if it continues the training loop; False if it stops
-    """
-
-    # Depending on the criteria for termination,
-    # change the return bool value
-
-    keep_running = True
-    client = kwargs.get('client')
-    current_fl_round = client.federated_training_round
-    print(f"Current Federated Learning Round: >>>>>> : {current_fl_round}")
-    if current_fl_round >= int(kwargs.get("round_to_exit")):
-        keep_running = False
-        client.stop_model_exchange_routine()
-    return keep_running
-
-
-if __name__ == '__main__':
-    # MLFLow
-
-    parser = argparse.ArgumentParser(description='STADLE Minimal Training')
-    parser.add_argument('--agent_name', default='default_agent')
-    args = parser.parse_args()
-
-    client_config_file = 'config/config_agent.json'
-
-    model = get_minimal_model()
-
-    integrated_client = IntegratedClient(config_file=client_config_file, simulation_flag=True, agent_name=args.agent_name)
-    integrated_client.maximum_rounds = 100000
-
-    integrated_client.set_termination_function(judge_termination, round_to_exit=20, client=integrated_client)
-
-    integrated_client.set_training_function(train, None)
-    integrated_client.set_validation_function(validate, None)
-    integrated_client.set_testing_function(test, None)
-
-    integrated_client.set_bm_obj(model)
-
-    integrated_client.start()
-    training_done = integrated_client.training_finalized
-    if training_done:
-        print("Training Completed!")
+stadle_client.disconnect()
